012199b4dcb886813ab6f60779be52a51d7182fe
Michael Akinde <michael.akinde@met.no>
2011-01-27 20:06:23 +0000

    Updated metadata.

--- etc/wdb_parameter.in.sql
+++ etc/wdb_parameter.in.sql
@@ -1,6 +1,19 @@
 SELECT wci.begin( 'wdb', 0, 0, 0 );
 -- MetNo Specific Parameters
+SELECT wci.addparameter( 'yr weather symbol', null, null, null, null, null, null, 'none' );
+SELECT wci.addparameter( 'probability of yr weather symbol', null, null, null, null, null, null, '%' );
 SELECT wci.addparameter( 'kalman air temperature', null, null, null, null, null, null, 'K' );
+SELECT wci.addparameter( 'air temperature', null, null, null, null, null, '10th percentile', 'K' );
+SELECT wci.addparameter( 'air temperature', null, null, null, null, null, '25th percentile', 'K' );
+SELECT wci.addparameter( 'air temperature', null, null, null, null, null, '50th percentile', 'K' );
+SELECT wci.addparameter( 'air temperature', null, null, null, null, null, '75th percentile', 'K' );
+SELECT wci.addparameter( 'air temperature', null, null, null, null, null, '90th percentile', 'K' );
+SELECT wci.addparameter( 'probability of wind speed', null, null, null, null, null, null, '%' );
+SELECT wci.addparameter( 'precipitation amount', null, null, null, null, null, '10th percentile', 'kg/m2' );
+SELECT wci.addparameter( 'precipitation amount', null, null, null, null, null, '25th percentile', 'kg/m2' );
+SELECT wci.addparameter( 'precipitation amount', null, null, null, null, null, '50th percentile', 'kg/m2' );
+SELECT wci.addparameter( 'precipitation amount', null, null, null, null, null, '75th percentile', 'kg/m2' );
+SELECT wci.addparameter( 'precipitation amount', null, null, null, null, null, '90th percentile', 'kg/m2' );
 SELECT wci.addparameter( 'precipitation amount', null, null, null, null, null, 'local 20th percentile', 'kg/m2' );
 SELECT wci.addparameter( 'precipitation amount', null, null, null, null, null, 'local 50th percentile', 'kg/m2' );
 SELECT wci.addparameter( 'precipitation amount', null, null, null, null, null, 'local 80th percentile', 'kg/m2' );
@@ -8,6 +21,15 @@ SELECT wci.addparameter( 'probability of precipitation', null, null, null, null,
 SELECT wci.addparameter( 'vessel icing index', null, null, null, null, null, null, 'none' );
 SELECT wci.addparameter( 'lwe thickness of precipitation amount', null, null, null, null, null, null, 'm' );
 SELECT wci.addparameter( 'unsmoothed land area fraction', null, null, null, null, null, null, '%' );
+SELECT wci.addparameter( 'probability of precipitation >0.1mm', null, null, null, null, null, null, '%' );
+SELECT wci.addparameter( 'probability of precipitation >0.2mm', null, null, null, null, null, null, '%' );
+SELECT wci.addparameter( 'probability of precipitation >0.5mm', null, null, null, null, null, null, '%' );
+SELECT wci.addparameter( 'probability of precipitation >1.0mm', null, null, null, null, null, null, '%' );
+SELECT wci.addparameter( 'probability of precipitation >2.0mm', null, null, null, null, null, null, '%' );
+SELECT wci.addparameter( 'probability of precipitation >5.0mm', null, null, null, null, null, null, '%' );
+SELECT wci.addparameter( 'probability of air temperature +/-1.5', null, null, null, null, null, null, '%' );
+SELECT wci.addparameter( 'probability of air temperature +/-2.5', null, null, null, null, null, null, '%' );
+
 
 -- Load all default parameters into namespace
 SELECT wci.begin( 'wdb', 88, 88, 88 );
