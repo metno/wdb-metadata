7a8f9a3f51cecf14b45fd9b3ec84496ee0a24d37
Michael Akinde <michael.akinde@met.no>
2012-03-02 13:55:45 +0100

    Added two new namespaces.

--- etc/wdb_namespace.in.sql
+++ etc/wdb_namespace.in.sql
@@ -1,5 +1,7 @@
 -- Define the Namespaces
 SELECT wci.addNameSpace( 88, 'Met.no standard namespace', 'The standard namespace used for Met.no applications that do not utilize the default namespace', 'production', 'met.no', 'MiA', '2000-01-01' );
+SELECT wci.addNameSpace( 88000, 'Climate number series (Met.no)', 'This is a namespace used for listing so-called climate number series used for internal identification of observation stations.', 'production', 'met.no', 'MiA', '2000-01-01' );
+SELECT wci.addNameSpace( 88001, 'WMO number series (Met.no)', 'This is a namespace used for listing WMO numbers. Each name in the namespace corresponds to a WMO number.', 'production', 'met.no', 'MiA', '2000-01-01' );
 
 -- Default Namespace of this installation
 SELECT wci.setDefaultNameSpace( '', 88, 88, 88 );
